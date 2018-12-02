6b5fb4f7641d439127083c57308636e3a4366ad3
dipranjan <dipranjan.chatterjee@gmail.com>
2018-12-02 11:23:35 +0530

    Base class sync up

--- Selenium-PlugAndPlay-Framework/src/test/java/base/BaseClass.java
+++ Selenium-PlugAndPlay-Framework/src/test/java/base/BaseClass.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package base;
 
 import java.io.FileNotFoundException;
@@ -9,6 +7,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,6 +16,10 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+import utilities.ExtentManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
@@ -25,13 +28,24 @@ public class BaseClass {
 	public static Logger log=Logger.getLogger("rootLogger");;	
 	public static Properties properties = new Properties();
 	
+	public ExtentReports rep = ExtentManager.getInstance();
+	public static ExtentTest test;
 	
-	@BeforeSuite(description="Framework initilization")
+	protected final static int qos = 2;
+	protected final static String topic = properties.getProperty("topic");
+	protected static final String serverUri = properties.getProperty("serverUri");;
+	protected static final String username = properties.getProperty("username");;
+    protected static final String password = properties.getProperty("password");;
+    protected static final String clientId = properties.getProperty("clientId");;
+    protected static MqttClient client;
+	
+	
+	@BeforeSuite(description="Suite started")
 	public static void BeforeSuite() throws FileNotFoundException, IOException {		
 		properties.load(new FileReader(System.getProperty("user.dir")+"\\src\\test\\resources\\configuration.properties"));		
 		log.debug("Logs initialized");
-	}
-	
+		log.debug("Suite Started");
+	}	
 	@BeforeTest(description="Driver initilization and pre test setup")
 	public void BeforeTest() {
 		//Setting up the browser as per specification in the configuration file.
@@ -94,7 +108,15 @@ public class BaseClass {
 	
 	@AfterTest(description="Driver teardown")
 	public void AfterTest() {
+		rep.endTest(test);
 		driver.quit();
 		log.debug("Driver quit.");
+	}
+	
+	@AfterSuite(description="Suite ended")
+	public void AfterSuite() {
+		rep.flush();
+		log.debug("Report Flushed");
+		log.debug("Suite ended");
 	}
 }
